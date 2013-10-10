2a42ab28d551cab35f0ef28e3a78326f4ea85b34
peteruithoven <peter@peteruithoven.nl>
2013-10-10 12:00:14 +0200

    Nicer loader indicator

--- js/main.js
+++ js/main.js
@@ -19,16 +19,16 @@ $(function() {
 	$preloader = $("#preloader");
 	
 	var spinnerSettings = {
-	  lines: 13, // The number of lines to draw
-	  length: 5, // The length of each line
-	  width: 3, // The line thickness
-	  radius: 7, // The radius of the inner circle
+	  lines: 7, // The number of lines to draw
+	  length: 0, // The length of each line
+	  width: 14, // The line thickness
+	  radius: 15, // The radius of the inner circle
 	  corners: 1, // Corner roundness (0..1)
 	  rotate: 0, // The rotation offset
 	  direction: 1, // 1: clockwise, -1: counterclockwise
 	  color: '#57BF42', // #rgb or #rrggbb or array of colors
-	  speed: 1, // Rounds per second
-	  trail: 60, // Afterglow percentage
+	  speed: 1.2, // Rounds per second
+	  trail: 69, // Afterglow percentage
 	  shadow: false, // Whether to render a shadow
 	  hwaccel: false, // Whether to use hardware acceleration
 	  className: 'spinner', // The CSS class to assign to the spinner
